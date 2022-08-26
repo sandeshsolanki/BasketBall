7ff1e8d3a2780979153508be244a77546c4e8799
DESKTOP-D8U6V2O\sony <sandeshsolanki4@gmail.com>
2022-08-26 11:29:15 +0530

    game of basketball

---
+++ Assets/Project/Scripts/player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+
+    private bool holdingBall = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // ball.GetComponent<Rigidbody>().useGravity = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall)
+        {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                
+                //ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce);
+            }
+
+        }
+    }
+}
